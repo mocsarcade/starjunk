d871218621a636033eae01981a787a85b556635b
Andrew McPherson <andrewmcp333@gmail.com>
2015-11-15 13:37:03 -0800

    Generated the projectiles from the onetapship.

--- source/index.js
+++ source/index.js
@@ -1,13 +1,17 @@
+"use strict"
+
 var Pixi = require("pixi.js")
 
-var renderer = Pixi.autoDetectRenderer(80, 45)
+var WIDTH = 80, HEIGHT = 45
+
+var renderer = Pixi.autoDetectRenderer(WIDTH, HEIGHT)
 document.getElementById("frame").appendChild(renderer.view)
 
-var makeImage = function(colors, pixels) {
+var Texture = function(colors, pixels) {
     var canvas = document.createElement("canvas")
     var context = canvas.getContext("2d")
 
-    canvas.width = pixels[0].length
+    canvas.width = pixels[pixels.length-1].length
     canvas.height = pixels.length
 
     for(var y = 0; y < pixels.length; y++) {
@@ -20,7 +24,7 @@ var makeImage = function(colors, pixels) {
         }
     }
 
-    return canvas.toDataURL()
+    return Pixi.Texture.fromImage(canvas.toDataURL())
 }
 
 var Colors = {
@@ -31,44 +35,91 @@ var Colors = {
     4: "#EEC584"
 }
 
-//var image = require("./image.png")
-var image = makeImage(Colors, [
-    [0,0,0,1,0,0,0],
-    [1,0,0,1,0,0,1],
-    [0,0,0,1,0,0,0],
-    [1,0,0,1,0,0,1],
-    [1,1,0,1,0,1,1],
-    [0,0,0,1,0,0,0],
-    [1,0,1,1,1,0,1],
-])
-
-var pulser = Pixi.Sprite.fromImage(image)
-pulser.position.x = 15.5
-pulser.position.y = 15.5
-pulser.anchor.x = 0.5
-pulser.anchor.y = 0.5
+var Images = {
+    "onetapship": Texture(Colors, [
+        [1,1,0,1,1, , , , , , ],
+        [0,1,0,0,1,1,0,0, , , ],
+        [1,1,0,0,0,0,0,1,0,0, ],
+        [0,1,0,0,1,0,1,0,0,1,1],
+        [1,1,0,1,1,0,0,1,0,0,1],
+    ]),
+    "projectile": Texture(Colors, [
+        [1,2,3,2]
+    ])
+}
 
 var stage = new Pixi.Container()
+
+class Onetapship extends Pixi.Sprite {
+    constructor() {
+        super(Images.onetapship)
+        this.position.x = 15.5
+        this.position.y = 15.5
+        this.anchor.x = 0.5
+        this.anchor.y = 0.5
+
+        this.speed = 500
+        this.firerate = 0
+        this.maxfirerate = 0.1
+    }
+    update(tick) {
+        if(Input.isDown("<space>")) {
+            if(this.position.y < HEIGHT - 15.5) {
+                this.position.y += this.speed * tick
+                if(this.position.y > HEIGHT - 15.5) {
+                    this.position.y = HEIGHT - 15.5
+                }
+            }
+        } else {
+            if(this.position.y > 15.5) {
+                this.position.y -= this.speed * tick
+                if(this.position.y < 15.5) {
+                    this.position.y = 15.5
+                }
+            }
+        }
+        if(this.position.y == 15.5
+        || this.position.y == HEIGHT - 15.5) {
+            this.firerate += tick
+            if(this.firerate > this.maxfirerate) {
+                this.firerate -= this.maxfirerate
+                this.parent.addChild(new Projectile({
+                    x: this.position.x + (this.width / 2),
+                    y: this.position.y + (this.height / 2) - 1
+                }))
+            }
+        }
+    }
+}
+
+var pulser = new Onetapship()
 stage.addChild(pulser)
+
+class Projectile extends Pixi.Sprite {
+    constructor(protoprojectile) {
+        super(Images.projectile)
+        this.position.x = protoprojectile.x || 0
+        this.position.y = protoprojectile.y || 0
+        this.anchor.y = 0.5
+
+        this.speed = 200
+    }
+    update(tick) {
+        this.position.x += this.speed * tick
+        if(this.position.x > WIDTH) {
+            this.parent.removeChild(this)
+        }
+    }
+}
 
 var Loop = require("./systems/Loop")
 var Input = require("./systems/Input")
 
 var loop = new Loop(function(tick) {
-    renderer.render(stage)
-    if(Input.isDown("<space>")) {
-        if(pulser.position.y < 45 - 15.5) {
-            pulser.position.y += 500 * tick
-            if(pulser.position.y > 45 - 15.5) {
-                pulser.position.y = 45 - 15.5
-            }
-        }
-    } else {
-        if(pulser.position.y > 15.5) {
-            pulser.position.y -= 500 * tick
-            if(pulser.position.y < 15.5) {
-                pulser.position.y = 15.5
-            }
+    for(var key in stage.children) {
+        if(!!stage.children[key].update) {
+            stage.children[key].update(tick)
         }
     }
+    renderer.render(stage)
 })
