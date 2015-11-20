e7cdb5e51083356dee5ba0a06482ea9e2e0c8209
Andrew McPherson <andrewmcp333@gmail.com>
2015-11-20 09:17:51 -0800

    Added a supercontainer and gameflow conditions.

--- source/index.js
+++ source/index.js
@@ -1,6 +1,7 @@
 var Pixi = require("pixi.js")
 var Keyb = require("keyb")
 var Loop = require("./systems/Loop")
+var ShortID = require("shortid")
 
 const WIDTH = 80
 const HEIGHT = 45
@@ -58,6 +59,35 @@ var Images = {
     ])),
 }
 
+class SuperContainer extends Pixi.Container {
+    constructor() {
+        super(arguments)
+    }
+    update(tick) {
+        for(var key in this.children) {
+            if(!!this.children[key].update) {
+                this.children[key].update(tick)
+            }
+        }
+    }
+    addChild(label, child) {
+        child = child || label
+        super.addChild(child)
+        if(label != child) {
+            child.label = label
+            this[label] = this[label] || {}
+            child.id = ShortID.generate()
+            this[label][child.id] = child
+        }
+    }
+    removeChild(child) {
+        super.removeChild(child)
+        if(!!child.label && !!this[child.label]) {
+            delete this[child.label][child.id]
+        }
+    }
+}
+
 class Defender extends Pixi.Sprite {
     constructor() {
         super(Images.defender)
@@ -76,7 +106,7 @@ class Defender extends Pixi.Sprite {
         || Keyb.isDown("<right>")) {
             this.position.x += this.speed * tick
         } if(Keyb.isJustDown("<space>")) {
-            stage.addChild(new Projectile({
+            stage.addChild("projectile", new Projectile({
                 x: this.position.x,
                 y: this.position.y
             }))
@@ -91,18 +121,22 @@ class Projectile extends Pixi.Sprite {
         this.position.y = HEIGHT - 6
         this.anchor.x = 0.5
         this.anchor.y = 1
-        this.speed = 200
+        this.speed = 50
     }
     update(tick) {
         this.position.y -= this.speed * tick
         if(this.position.y < 0) {
              this.parent.removeChild(this)
         }
-        for(var key in invaders.children) {
-            var invader = invaders.children[key]
+        for(var key in stage.invaders) {
+            var invader = stage.invaders[key]
             if(Math.abs(invader.position.y - this.position.y) < 8
             && Math.abs(invader.position.x - this.position.x) < 6) {
-                invaders.removeChild(invader)
+                stage.removeChild(this)
+                stage.removeChild(invader)
+                if(Object.keys(stage.invaders).length == 0) {
+                    console.log("You win!")
+                }
             }
         }
     }
@@ -110,30 +144,54 @@ class Projectile extends Pixi.Sprite {
 
 class Invader extends Pixi.Sprite {
     constructor(protoinvader) {
-        super(Images.invader2)
+        super(protoinvader.image)
         this.position.x = protoinvader.x
         this.position.y = protoinvader.y
         this.anchor.x = 0.5
         this.anchor.y = 0.5
+
+        this.maxstrafe = protoinvader.strafe
+        this.direction = +1
+        this.strafe = 0
+        this.speed = 20
+    }
+    update(tick) {
+        this.position.x += this.speed * this.direction * tick
+        this.strafe += this.speed * tick
+        if(this.strafe > this.maxstrafe) {
+            this.strafe -= this.maxstrafe
+            this.direction *= -1
+            this.position.y += 2
+            if(this.position.y > HEIGHT - 16) {
+                console.log("You lose!")
+            }
+        }
     }
 }
 
-var stage = new Pixi.Container()
+var stage = new SuperContainer()
 stage.addChild(new Defender())
-var invaders = new Pixi.Container()
-stage.addChild(invaders)
-invaders.addChild(new Invader({
-    x: 6+8, y: 4+8
+
+stage.addChild("invaders", new Invader({
+    x: 6+8, y: 4+8,
+    image: Images.invader1,
+    strafe: 24
 }))
-invaders.addChild(new Invader({
-    x: 6+8+11+3, y: 4+8
+stage.addChild("invaders", new Invader({
+    x: 6+8+11+3, y: 4+8,
+    image: Images.invader2,
+    strafe: 24
+}))
+stage.addChild("invaders", new Invader({
+    x: 6+8+((11+3)*2), y: 4+8,
+    image: Images.invader1,
+    strafe: 24
 }))
 
 var loop = new Loop(function(tick) {
-    for(var key in stage.children) {
-        if(!!stage.children[key].update) {
-            stage.children[key].update(tick)
-        }
-    }
+    stage.update(tick)
     renderer.render(stage)
 })
+
+// use tickly for loop
+// better update loop
