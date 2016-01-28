731df2df7f36bc634a04675bfe9ce4846b276353
Skylar Rowan Lolmaugh <s.r.lolmaugh@gmail.com>
2016-01-28 00:03:51 -0500

    Failed to push some files in previous commit. Don't git while sleepy, kids.

--- source/index.js
+++ source/index.js
@@ -2,15 +2,22 @@ var Pixi = require("pixi.js")
 var Afloop = require("afloop")
 var Keyb = require("keyb")
 
-var WIDTH = 230, HEIGHT = 130
+import Player from "./player.js"
 
-var renderer = Pixi.autoDetectRenderer(WIDTH, HEIGHT)
+window.GAME_WIDTH = 230
+window.GAME_HEIGHT = 130
+
+var renderer = Pixi.autoDetectRenderer(GAME_WIDTH, GAME_HEIGHT)
 renderer.backgroundColor = 0x222222
 renderer.roundPixels = true
 
 document.getElementById("mount").appendChild(renderer.view)
+window.game = new Pixi.Container()
+game.addChild(new Player())
 
-var game = new Pixi.Container()
 var loop = new Afloop(function(delta) {
+    game.children.forEach((child) => {
+        child.update(delta)
+    })
     renderer.render(game)
 })
