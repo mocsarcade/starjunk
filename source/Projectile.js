663f74e2c1152bda11ac9aa0473e1a82f8932781
HolySchmitt <xgm537@mocs.utc.edu>
2016-04-20 23:15:28 -0400

    works now

--- source/Projectile.js
+++ source/Projectile.js
@@ -72,8 +72,8 @@ export default class Projectile extends Pixi.Sprite {
           this.rotation += 1
       }
 
-      if(this.projectileType == "superMine") {
-          this.scale.x = 1
+      if(this.projectileType == "mine") {
+          this.scale.x = 2
       }
 
       if(this.projectileType == "superMine") {
@@ -87,11 +87,9 @@ export default class Projectile extends Pixi.Sprite {
       if(this.projectileType == "mine") {
           this.onHit.explodeEnemy(this)
           this.destroy()
-          collidedWith.destroy()
       } else if(this.projectileType == "superMine") {
           this.onHit.explodeEnemy(this)
           this.destroy()
-          collidedWith.destroy()
       } else if(this.piercing != true) {
           this.destroy()
       } else {
